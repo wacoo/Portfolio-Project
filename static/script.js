b1fcf8cb77ca0f779926d6950fa90ad66a488e1a
wac <wabaham9@gmail.com>
2023-06-26 17:27:01 +0300

    Add links to menu items

--- static/script.js
+++ static/script.js
@@ -2,14 +2,6 @@ const modal_menu = document.querySelector('.modal-1');
 const btn_hamburger = document.querySelector('.menu');
 const btn_close = document.querySelector('.close-1');
 
-
-const links = document.querySelectorAll('.lnks a');
-for (let link of links) {
-    link.addEventListener('click', () => {
-        modal_menu.close();
-    });
-}
-
 btn_hamburger.addEventListener('click', () => {
     modal_menu.show();
 });
