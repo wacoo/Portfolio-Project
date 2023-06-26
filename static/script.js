a41623db2c96fbf3217f874a3319db19dfcdfb4a
Ezinwa charles <charleskelvin12@gmail.com>
2023-06-26 14:33:04 +0100

    Add script

---
+++ static/script.js
@@ -0,0 +1,11 @@
+const modal_menu = document.querySelector('.modal-1');
+const btn_hamburger = document.querySelector('.menu');
+const btn_close = document.querySelector('.close');
+
+btn_hamburger.addEventListener('click', () => {
+    modal_menu.show();
+});
+
+btn_close.addEventListener('click', () => {
+    modal_menu.close();
+})
